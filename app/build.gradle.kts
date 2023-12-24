bd96a3979a90628806c93359f1115dd2cae7c2c8
Oscar <rkdgjdl@gmail.com>
2023-12-25 03:27:40 +0900

    Build : build.gradle.kts(Module:app) 수정
    Retrofit 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+    // Basic
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -47,4 +48,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Scalars (Converter)
+    implementation("com.squareup.retrofit2:converter-scalars:2.6.4")
+
+    // Gson (Converter)
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
