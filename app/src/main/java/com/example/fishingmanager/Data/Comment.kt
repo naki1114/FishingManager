55dd40d186f990bbf26a41cacbfcdad9a6e8fd85
Naki <naki1114@naver.com>
2023-12-30 22:01:16 +0900

    Chore : feedNum 변수 String -> Int로 수정

--- app/src/main/java/com/example/fishingmanager/Data/Comment.kt
+++ app/src/main/java/com/example/fishingmanager/Data/Comment.kt
@@ -2,7 +2,7 @@ package com.example.fishingmanager.Data
 
 data class Comment(
     val nickname: String,
-    val feedNum: String,
+    val feedNum: Int,
     val content: String,
     val date: String
 )
