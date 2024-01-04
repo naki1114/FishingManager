c6975d400a3cbc7ab844b34e73fd13c573732a86
Naki <naki1114@naver.com>
2024-01-04 15:57:36 +0900

    Chore : POST 방식 Retrofit에 FromUrlEncoded 추가

--- app/src/main/java/com/example/fishingmanager/network/RetrofitInterface.kt
+++ app/src/main/java/com/example/fishingmanager/network/RetrofitInterface.kt
@@ -11,6 +11,7 @@ import com.example.fishingmanager.data.Weather
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -50,21 +51,25 @@ interface RetrofitInterface {
 
 
     // 도감, 기록, 게시글, 사용자 정보 조회
+    @FormUrlEncoded
     @POST("File/Splash/GetUserData.php")
     fun requestDB(@Field("nickname") nickname: String) : Call<Combine>
 
 
     // 이메일 중복 체크
+    @FormUrlEncoded
     @POST("File/UserInfo/EmailDuplicateCheck.php")
     fun checkUserEmail(@Field("id") id: String): Call<String>
 
 
     // 닉네임 중복 체크
+    @FormUrlEncoded
     @POST("File/UserInfo/NicknameDuplicateCheck.php")
     fun checkUserNickname(@Field("nickname") nickname: String): Call<String>
 
 
     // 회원가입 완료
+    @FormUrlEncoded
     @POST("File/UserInfo/FinishSignup.php")
     fun saveUserInfo(
         @Field("id") id: String,
@@ -74,6 +79,7 @@ interface RetrofitInterface {
 
 
     // 비밀번호 찾기
+    @FormUrlEncoded
     @POST("File/UserInfo/FindPassword.php")
     fun changePassword(
         @Field("id") id: String,
@@ -82,6 +88,7 @@ interface RetrofitInterface {
 
 
     // 로그인 정보 조회
+    @FormUrlEncoded
     @POST("File/UserInfo/LoginCheck.php")
     fun checkUserInfo(
         @Field("id") id: String,
@@ -90,12 +97,14 @@ interface RetrofitInterface {
 
 
     // 회원탈퇴
+    @FormUrlEncoded
     @POST("File/UserInfo/DeleteAccount.php")
     fun deleteUserInfo(@Field("nickname") nickname: String): Call<String>
 
 
     // 프로필 사진 수정
     @Multipart
+    @FormUrlEncoded
     @POST("File/UserInfo/UpdateProfileImage.php")
     fun updateProfileImage(
         @Part uploadFile: MultipartBody.Part,
@@ -104,12 +113,14 @@ interface RetrofitInterface {
 
 
     // 게시글 요청
+    @FormUrlEncoded
     @POST("File/Feed/GetFeed.php")
     fun getFeed(): Call<ArrayList<Feed>>
 
 
     // 게시글 추가
     @Multipart
+    @FormUrlEncoded
     @POST("File/Feed/InsertFeed.php")
     fun insertFeed(
         @Part uploadFile: MultipartBody.Part,
@@ -123,6 +134,7 @@ interface RetrofitInterface {
 
     // 게시글 수정
     @Multipart
+    @FormUrlEncoded
     @POST("File/Feed/UpdateFeed.php")
     fun updateFeed(
         @Part uploadFile: MultipartBody.Part,
@@ -135,6 +147,7 @@ interface RetrofitInterface {
 
 
     // 게시글 삭제
+    @FormUrlEncoded
     @POST("File/Feed/DeleteFeed.php")
     fun deleteFeed(
         @Field("nickname") nickname: String,
@@ -143,11 +156,13 @@ interface RetrofitInterface {
 
 
     // 댓글 요청
+    @FormUrlEncoded
     @POST("File/Feed/GetComment.php")
     fun getComment(): Call<ArrayList<Comment>>
 
 
     // 댓글 추가
+    @FormUrlEncoded
     @POST("File/Feed/InsertComment.php")
     fun insertComment(
         @Field("nickname") nickname: String,
@@ -158,6 +173,7 @@ interface RetrofitInterface {
 
 
     // 댓글 삭제
+    @FormUrlEncoded
     @POST("File/Feed/DeleteComment.php")
     fun deleteComment(
         @Field("nickname") nickname: String,
@@ -167,6 +183,7 @@ interface RetrofitInterface {
 
 
     // 도감 조회
+    @FormUrlEncoded
     @POST("File/CheckingFish/CheckCollection.php")
     fun checkCollection(
         @Field("nickname") nickname: String,
@@ -178,6 +195,7 @@ interface RetrofitInterface {
 
     // 기록 추가
     @Multipart
+    @FormUrlEncoded
     @POST("File/CheckingFish/InsertHistory.php")
     fun saveHistory(
         @Part uploadFile: MultipartBody.Part,
@@ -189,12 +207,12 @@ interface RetrofitInterface {
 
 
     // 이용권 구매
+    @FormUrlEncoded
     @POST("File/UserInfo/UpdateTicket.php")
     fun updateTicket(
         @Field("nickname") nickname: String,
         @Field("product") product: String,
         @Field("dueDate") dueDate: String
     ): Call<String>
-
 
 }
