eaaa2a9e032d9b3b879bebee7c2b14b51bc3864d
Naki <naki1114@naver.com>
2024-01-25 07:03:13 +0900

    Chore : WriteFragment 기능 추가
    - 뒤로가기 버튼 클릭 시 FeedFragment로 이동
    - 이미지 추가 버튼 클릭 시 갤러리로 이동 후 이미지 가져오기
    - 제목 / 내용 입력 없이 완료 버튼 클릭 시 토스트 메시지
    - 완료 버튼 클릭 시 게시글 저장 (이미지 업로드 제외)

--- app/src/main/java/com/example/fishingmanager/model/WriteModel.kt
+++ app/src/main/java/com/example/fishingmanager/model/WriteModel.kt
@@ -1,4 +1,12 @@
 package com.example.fishingmanager.model
 
+import com.example.fishingmanager.network.RetrofitClient
+import com.example.fishingmanager.network.RetrofitInterface
+
 class WriteModel {
+
+    private val responseServer = RetrofitClient.getWebServer().create(RetrofitInterface::class.java)
+
+    fun insertFeed(nickname : String, title : String, content : String, date : Int) = responseServer.insertFeed(nickname, title, content, date)
+
 }
--- app/src/main/java/com/example/fishingmanager/viewModel/WriteViewModel.kt
+++ app/src/main/java/com/example/fishingmanager/viewModel/WriteViewModel.kt
@@ -1,4 +1,162 @@
 package com.example.fishingmanager.viewModel
 
-class WriteViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.fishingmanager.model.WriteModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class WriteViewModel : ViewModel() {
+
+    val TAG = "WriteViewModel"
+
+    val isBackLayoutLiveData = MutableLiveData<String>()
+    val isCheck = MutableLiveData<Boolean>()
+
+    val titleStatus = MutableLiveData<String>()
+    val contentStatus = MutableLiveData<String>()
+
+    val toGalleryLiveData = MutableLiveData<Boolean>()
+
+    private var model = WriteModel()
+
+    lateinit var title : String
+    lateinit var content : String
+    lateinit var nickname : String
+
+    fun writeBackLayout(type : String) {
+
+        isBackLayoutLiveData.value = type
+
+    } // writeBackLayout
+
+    fun writeBackLayout(type : String, title : String, content : String) {
+
+        if (title.isEmpty()) {
+            titleStatus.value = "empty"
+            isBackLayoutLiveData.value = "empty"
+        }
+        else if (title.length > 20) {
+            titleStatus.value = "over"
+        }
+        else if (content.isEmpty()) {
+            Log.d(TAG, "writeBackLayout : 눌렀잖아")
+            contentStatus.value = "empty"
+            isBackLayoutLiveData.value = "empty"
+        }
+        else if (content.length > 1000) {
+            contentStatus.value = "over"
+        }
+        else {
+            titleStatus.value = title
+            contentStatus.value = content
+            isBackLayoutLiveData.value = type
+        }
+
+    }
+
+    fun clickCheck() {
+
+        when(isBackLayoutLiveData.value) {
+
+            "back" -> {
+                isCheck.value = true
+            }
+            "complete" -> {
+                insertFeed()
+                isCheck.value = true
+            }
+
+        }
+
+    } // clickCheck
+
+    fun clickCancel() {
+
+        isCheck.value = false
+
+    } // clickCancel
+
+    fun titleChanged(title : String) {
+
+        this.title = title
+
+        if (title.isEmpty()) {
+            titleStatus.value = "empty"
+            isBackLayoutLiveData.value = "empty"
+        }
+        else if (title.length > 20) {
+            titleStatus.value = "over"
+            isBackLayoutLiveData.value = null
+        }
+        else {
+            titleStatus.value = title
+            isBackLayoutLiveData.value = null
+        }
+
+    } // titleChanged
+
+    fun contentChanged(content : String) {
+
+        this.content = content
+
+        if (content.isEmpty()) {
+            contentStatus.value = "empty"
+            isBackLayoutLiveData.value = "empty"
+        }
+        else if (content.length > 1000) {
+            contentStatus.value = "over"
+            isBackLayoutLiveData.value = null
+        }
+        else {
+            contentStatus.value = content
+            isBackLayoutLiveData.value = null
+        }
+
+    } // contentChanged
+
+    private fun insertFeed() {
+
+        val date = (System.currentTimeMillis() / 1000).toInt()
+
+//        model.insertFeed(nickname, title, content, date).enqueue(object : Callback<ArrayList<Feed>> {
+//
+//            override fun onResponse(call: Call<ArrayList<Feed>>, response: Response<ArrayList<Feed>>) {
+//                if (response.isSuccessful) {
+//                    var msg : ArrayList<Feed>? = response.body()
+//                    Log.d(TAG, "onResponse : $msg")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<ArrayList<Feed>>, t: Throwable) {
+//                Log.d(TAG, "onFailure : ${t.message}")
+//            }
+//
+//        })
+
+        model.insertFeed(nickname, title, content, date).enqueue(object : Callback<String> {
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    var msg : String? = response.body()
+                    Log.d(TAG, "onResponse : $msg")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.d(TAG, "onFailure : ${t.message}")
+            }
+
+        })
+
+    }
+
+    fun toGallery() {
+
+        toGalleryLiveData.value = true
+
+    }
+
 }
