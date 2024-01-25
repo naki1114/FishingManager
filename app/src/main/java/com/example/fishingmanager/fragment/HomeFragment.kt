227a978ffb4ac9d02514d262bc1d5b03abf5cd92
Oscar <rkdgjdl@gmail.com>
2024-01-25 15:16:14 +0900

    Add : Add ViewModel & Model about HomeFragment

--- app/src/main/java/com/example/fishingmanager/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/HomeFragment.kt
@@ -1,60 +1,85 @@
 package com.example.fishingmanager.fragment
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.fishingmanager.R
+import com.example.fishingmanager.activity.MainActivity
+import com.example.fishingmanager.databinding.FragmentHomeBinding
+import com.example.fishingmanager.viewModel.HomeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentHomeBinding
+    lateinit var viewModel: HomeViewModel
+
+    lateinit var locationShared: SharedPreferences
+    lateinit var location: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setVariable()
+        setView()
+        observeLiveData()
+        viewModel.getWeather()
+
+    } // onViewCreated()
+
+
+    fun setVariable() {
+
+        viewModel = HomeViewModel((activity as MainActivity).getWeatherList())
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        checkSharedPreference()
+
+    } // setVariable()
+
+
+    fun setView() {
+
+
+
+    } // setView()
+
+
+    fun observeLiveData() {
+
+
+
+    } // observeLiveData()
+
+
+    fun checkSharedPreference() {
+
+        locationShared = requireActivity().getSharedPreferences("location", AppCompatActivity.MODE_PRIVATE)
+
+        location = locationShared.getString("location", "").toString()
+
+        if (location == "") {
+            location = "영흥도"
+        }
+
     }
+
+
 }
