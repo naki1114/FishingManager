9ee7c51ff38a00bab3ee4abda4803cb2c53bf1c9
Oscar <rkdgjdl@gmail.com>
2024-01-25 14:28:30 +0900

    Docs : Modify Layout about HomeFragment

---
+++ app/src/main/java/com/example/fishingmanager/data/HomeWeather.kt
@@ -0,0 +1,11 @@
+package com.example.fishingmanager.data
+
+data class HomeWeather(
+    var time: String,
+    var skyImage: Int,
+    var temp: String,
+    var humidity: String,
+    var windSpeed: String,
+    var date: String,
+    var location: String
+)
