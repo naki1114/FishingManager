fb72ecab124c711834c7c2cc1e8eb40c93955aa8
Oscar <rkdgjdl@gmail.com>
2024-01-28 18:12:45 +0900

    Add : Add RecyclerView ItemLayout Resource

--- app/src/main/java/com/example/fishingmanager/data/Collection.kt
+++ app/src/main/java/com/example/fishingmanager/data/Collection.kt
@@ -3,6 +3,7 @@ package com.example.fishingmanager.data
 data class Collection(
     val nickname: String,
     val fishName: String,
+    val fishImage : Int,
     val fishLength: String,
     val date: String
 )
