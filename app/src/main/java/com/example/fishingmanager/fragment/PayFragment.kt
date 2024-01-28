1e26f53683b3cd9aff97a54616b70a44b08976e7
Oscar <rkdgjdl@gmail.com>
2024-01-28 16:39:29 +0900

    Add : PayTicket 어댑터, PayViewModel 추가

--- app/src/main/java/com/example/fishingmanager/fragment/PayFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/PayFragment.kt
@@ -5,56 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.fishingmanager.R
+import com.example.fishingmanager.adapter.PayTicketAdapter
+import com.example.fishingmanager.databinding.FragmentPayBinding
+import com.example.fishingmanager.viewModel.PayViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PayFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PayFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding : FragmentPayBinding
+    lateinit var viewModel : PayViewModel
+
+    lateinit var adapter : PayTicketAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pay, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PayFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PayFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pay, container, false)
+
+        return binding.root
+
+    } // onCreateView()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setVariable()
+        observeLiveData()
+
+    } // onViewCreated()
+
+
+    fun setVariable() {
+
+        viewModel = PayViewModel()
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        adapter = PayTicketAdapter(PayTicketAdapter.ItemClickListener {
+            viewModel.startPayStep(it)
+        })
+        binding.payRecyclerView.adapter = adapter
+
+    } // setVariable()
+
+
+    fun observeLiveData() {
+
+        viewModel.liveDataBackStatus.observe(viewLifecycleOwner, Observer {
+
+            if (it) {
+
+                requireActivity().finish()
+
             }
-    }
+
+        })
+
+        viewModel.liveDataProduct.observe(viewLifecycleOwner, Observer {
+
+            // 티켓 종류 구분하여 결제 진행 추가
+
+        })
+
+    } // observeLiveData()
+
 }
