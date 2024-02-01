55a54d299a564f8e1664665ca51d9ac70fc5891d
Naki <naki1114@naver.com>
2024-02-01 14:30:34 +0900

    Chore : Feed Fragment 기능 추가
    - 게시글 RecyclerView 추가
    - Spinner와 검색창 입력 keyword에 따른 RecyclerView 변경
    - 게시글 추가 버튼 클릭시 WriteFragment로 전환
    - 게시글 클릭시 게시글 읽기 화면으로 전환

--- app/src/main/java/com/example/fishingmanager/fragment/FeedFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/FeedFragment.kt
@@ -8,14 +8,18 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.fishingmanager.R
 import com.example.fishingmanager.activity.MainActivity
+import com.example.fishingmanager.adapter.FeedAdapter
 import com.example.fishingmanager.data.Feed
 import com.example.fishingmanager.databinding.FragmentFeedBinding
 import com.example.fishingmanager.viewModel.FeedViewModel
 
-class FeedFragment() : Fragment() {
+class FeedFragment : Fragment() {
 
     private lateinit var feedViewModel : FeedViewModel
     private lateinit var binding : FragmentFeedBinding
+    private lateinit var adapter : FeedAdapter
+
+    private lateinit var feedList : ArrayList<Feed>
 
     // Lifecycle
 
@@ -32,14 +36,28 @@ class FeedFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        feedViewModel = FeedViewModel((activity as MainActivity).feedList)
-        binding.viewModel = feedViewModel
+        setVariable()
 
         // LiveData Observe
         clickAddFeedButton()
+        readFeed()
+        toFeedList()
+        searchKeyword()
     }
 
     // Custom Method
+
+    private fun setVariable() {
+
+        feedViewModel = FeedViewModel((activity as MainActivity).feedList)
+        binding.viewModel = feedViewModel
+
+        adapter = FeedAdapter(FeedAdapter.ItemClickListener {
+            feedViewModel.readFeed(it)
+        }, (activity as MainActivity).feedList)
+        binding.feedRecyclerView.adapter = adapter
+
+    } // setVariable
 
     private fun clickAddFeedButton() {
 
@@ -53,6 +71,74 @@ class FeedFragment() : Fragment() {
 
         }
 
-    }
+    } // clickAddFeedButton
+
+    private fun readFeed() {
+
+        feedViewModel.toReadLiveData.observe(viewLifecycleOwner) {
+
+            val feed = feedViewModel.toReadLiveData.value
+
+            binding.feedLayout.visibility = View.GONE
+            binding.feedViewLayout.visibility = View.VISIBLE
+
+            binding.feedViewTitleTextView.text = feed?.title
+
+        }
+
+    } // readFeed
+
+    private fun toFeedList() {
+
+        feedViewModel.toFeedListLiveData.observe(viewLifecycleOwner) {
+
+            binding.feedLayout.visibility = View.VISIBLE
+            binding.feedViewLayout.visibility = View.GONE
+
+        }
+
+    } // toFeedList
+
+    private fun searchKeyword() {
+
+        feedViewModel.searchLiveData.observe(viewLifecycleOwner) {
+
+            feedList = ArrayList<Feed>()
+            val type = feedViewModel.searchLiveData.value?.get(0)
+            val keyword = feedViewModel.searchLiveData.value?.get(1)
+            val originalList = (activity as MainActivity).feedList
+            val size = originalList.size
+
+            for (i in 0 ..< size) {
+
+                if (type == "제목") {
+
+                    if (originalList[i].title.contains(keyword.toString())) {
+
+                        feedList.add(originalList[i])
+
+                    }
+
+                }
+                else {
+
+                    if (originalList[i].nickname == keyword) {
+
+                        feedList.add(originalList[i])
+
+                    }
+
+                }
+
+            }
+
+            adapter = FeedAdapter(FeedAdapter.ItemClickListener {
+                feedViewModel.readFeed(it)
+            }, feedList)
+            binding.feedRecyclerView.adapter = adapter
+
+        }
+
+    } // searchKeyword
 
 }
