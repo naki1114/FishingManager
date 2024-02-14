a75b5397d81ba8d04d78148d98e32b211ae9e496
Oscar <rkdgjdl@gmail.com>
2024-02-14 13:08:13 +0900

    Chore : 앱 진입 스플래시 수정 & 애니메이션 적용

--- app/src/main/java/com/example/fishingmanager/fragment/EnterFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/EnterFragment.kt
@@ -5,35 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.fishingmanager.R
 import com.example.fishingmanager.activity.MainActivity
+import com.example.fishingmanager.databinding.FragmentEnterBinding
 import kotlin.concurrent.thread
 
 class EnterFragment : Fragment() {
 
+    lateinit var binding : FragmentEnterBinding
     lateinit var nickname: String
+    lateinit var animation : Animation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_enter, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_enter, container, false)
+        return binding.root
 
     } // onCreateView()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setVariable()
         enterThread.start()
 
     } // onViewCreated()
 
+    fun setVariable() {
+
+        animation = AnimationUtils.loadAnimation(activity, R.anim.splash_alpha)
+        binding.enterImage.startAnimation(animation)
+
+    }
+
 
     val enterThread = thread(false) {
 
-        Thread.sleep(1000)
+        Thread.sleep(2500)
         (activity as MainActivity).changeFragment("start")
 
     }
