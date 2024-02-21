a80fc584baba5fd94b41dabdbd042aeef6e2f164
Naki <naki1114@naver.com>
2024-02-21 23:37:47 +0900

    Chore : 댓글 관련 기능 추가
    - 게시글 상세보기 시 댓글 RecyclerView 띄워주기
    - 댓글 작성 시 댓글 서버로 전송하고 댓글 리스트 업데이트

--- app/src/main/java/com/example/fishingmanager/viewModel/FeedViewModel.kt
+++ app/src/main/java/com/example/fishingmanager/viewModel/FeedViewModel.kt
@@ -3,13 +3,14 @@ package com.example.fishingmanager.viewModel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.fishingmanager.data.Comment
 import com.example.fishingmanager.data.Feed
 import com.example.fishingmanager.model.FeedModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel(val nickname : String) : ViewModel() {
 
     private var TAG = "FeedViewModel"
 
@@ -18,8 +19,11 @@ class FeedViewModel : ViewModel() {
     val toFeedListLiveData = MutableLiveData<Boolean>()
     val searchLiveData = MutableLiveData<ArrayList<String>>()
     val feedList = MutableLiveData<ArrayList<Feed>>()
+    val feedCommentList = MutableLiveData<ArrayList<Comment>>()
 
     private val model = FeedModel()
+
+    lateinit var feed : Feed
 
     fun searchFeed(type : String, keyword : String) {
         val search = arrayListOf(type, keyword)
@@ -27,6 +31,8 @@ class FeedViewModel : ViewModel() {
     } // searchFeed
 
     fun readFeed(feed : Feed) {
+
+        this.feed = feed
 
         model.updateViewCount(feed.nickname, feed.date).enqueue(object : Callback<ArrayList<Feed>> {
 
@@ -39,6 +45,22 @@ class FeedViewModel : ViewModel() {
             }
 
             override fun onFailure(call: Call<ArrayList<Feed>>, t: Throwable) {
+
+            }
+
+        })
+
+        model.getComment(feed.feedNum).enqueue(object : Callback<ArrayList<Comment>> {
+
+            override fun onResponse(call: Call<ArrayList<Comment>>, response: Response<ArrayList<Comment>>) {
+                if (response.isSuccessful) {
+                    var msg = response.body()
+
+                    feedCommentList.value = msg
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<Comment>>, t: Throwable) {
 
             }
 
@@ -78,6 +100,33 @@ class FeedViewModel : ViewModel() {
 
         })
 
-    }
+    } // getFeed
+
+    fun insertComment(content : String) {
+
+        val feedNum = feed.feedNum.toString()
+        val date = System.currentTimeMillis().toString()
+
+        if (content.isNotEmpty()) {
+
+            model.insertComment(nickname, feedNum, content, date).enqueue(object : Callback<ArrayList<Comment>> {
+
+                override fun onResponse(call : Call<ArrayList<Comment>>, response : Response<ArrayList<Comment>>) {
+                    if (response.isSuccessful) {
+                        var msg = response.body()
+
+                        feedCommentList.value = msg
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<Comment>>, t: Throwable) {
+                    Log.d(TAG, "onFailure")
+                }
+
+            })
+
+        }
+
+    } // insertComment
 
 }
