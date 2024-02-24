d5ee65a87e7a2df94c4d8a8979af319cbde75bd6
Oscar <rkdgjdl@gmail.com>
2024-02-24 22:23:22 +0900

    Add : 카메라 & 크롭 기능 추가, 텐서플로우 모델 수정

--- app/src/main/java/com/example/fishingmanager/fragment/CheckingFishFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/CheckingFishFragment.kt
@@ -1,11 +1,15 @@
 package com.example.fishingmanager.fragment
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,16 +17,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Guideline
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import com.canhub.cropper.CropImage
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
+import com.canhub.cropper.CropImageView.Guidelines
+import com.canhub.cropper.CropImageView.Guidelines.*
 import com.example.fishingmanager.R
+import com.example.fishingmanager.activity.CameraActivity
 import com.example.fishingmanager.activity.MainActivity
 import com.example.fishingmanager.adapter.CheckingFishHistoryAdapter
 import com.example.fishingmanager.databinding.FragmentCheckingFishBinding
 import com.example.fishingmanager.function.GetDate
 import com.example.fishingmanager.tensorflowModel.TensorflowModel
 import com.example.fishingmanager.viewModel.CheckingFishViewModel
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.websitebeaver.documentscanner.DocumentScanner
 import okhttp3.MediaType
 import okhttp3.MultipartBody
@@ -34,23 +56,41 @@ import kotlin.concurrent.thread
 class CheckingFishFragment : Fragment() {
 
     val TAG = "CheckingFishFragment"
-    lateinit var binding : FragmentCheckingFishBinding
+    lateinit var binding: FragmentCheckingFishBinding
 
-    lateinit var tensorflowModel : TensorflowModel
-    lateinit var viewModel : CheckingFishViewModel
-    lateinit var adapter : CheckingFishHistoryAdapter
+    lateinit var tensorflowModel: TensorflowModel
+    lateinit var viewModel: CheckingFishViewModel
+    lateinit var adapter: CheckingFishHistoryAdapter
 
-    lateinit var userInfoShared : SharedPreferences
-    lateinit var nickname : String
+    lateinit var userInfoShared: SharedPreferences
+    lateinit var nickname: String
 
     lateinit var loadingAnimationRight: Animation
     lateinit var loadingAnimationLeft: Animation
     var loadingAnimationStatus = false
     lateinit var animationThread: Thread
-    lateinit var resultBitmap : Bitmap
+    lateinit var resultBitmap: Bitmap
     lateinit var result: Pair<String, Float>
-    lateinit var file : File
-    lateinit var camera : DocumentScanner
+    lateinit var file: File
+
+    val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback<ActivityResult>() {
+
+        if (it.resultCode == 819) {
+
+            val intent = it.data
+            Log.d(TAG, "${it.resultCode}")
+            Log.d(TAG, "${intent!!.getStringExtra("image")}")
+//            binding.checkingFishCameraButton.setImageBitmap(BitmapFactory.decodeFile(intent!!.getStringExtra("image")))
+            binding.checkingFishCheckImageLayout.visibility = View.VISIBLE
+            binding.checkingFishMainLayout.visibility = View.GONE
+            resultBitmap = BitmapFactory.decodeFile(intent!!.getStringExtra("image"))
+            binding.checkingFishCheckImage.setImageBitmap(resultBitmap)
+
+        }
+
+    })
+
+    lateinit var captureUri : Uri
 
 
     override fun onCreateView(
@@ -58,7 +98,8 @@ class CheckingFishFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_checking_fish, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_checking_fish, container, false)
 
         return binding.root
 
@@ -78,23 +119,6 @@ class CheckingFishFragment : Fragment() {
 
     fun setVariable() {
 
-        camera = DocumentScanner(
-            requireActivity(),
-            {
-                    croppedImageResults ->
-                resultBitmap = BitmapFactory.decodeFile(croppedImageResults.first())
-                changeLayout("checkImage")
-                binding.checkingFishCheckImage.setImageBitmap(resultBitmap)
-            },
-            {
-                    errorMessage ->
-                Log.d(TAG, "cameraError : $errorMessage")
-            },
-            {
-                // 사용자가 카메라 취소했을 때
-            }
-        )
-
         tensorflowModel = TensorflowModel(requireActivity())
         viewModel = CheckingFishViewModel((activity as MainActivity).historyList, nickname)
         viewModel.init()
@@ -108,7 +132,8 @@ class CheckingFishFragment : Fragment() {
 
         binding.checkingFishHistoryRecyclerView.adapter = adapter
 
-        loadingAnimationRight = AnimationUtils.loadAnimation(activity, R.anim.loading_animation_right)
+        loadingAnimationRight =
+            AnimationUtils.loadAnimation(activity, R.anim.loading_animation_right)
         loadingAnimationLeft = AnimationUtils.loadAnimation(activity, R.anim.loading_animation_left)
 
     } // setVariable()
@@ -247,7 +272,9 @@ class CheckingFishFragment : Fragment() {
         viewModel.liveDataCameraStatus.observe(viewLifecycleOwner, Observer {
 
             if (it) {
+
                 startCamera()
+
             }
 
         })
@@ -277,7 +304,12 @@ class CheckingFishFragment : Fragment() {
         viewModel.liveDataSaveStatus.observe(viewLifecycleOwner, Observer {
 
             if (it) {
-                viewModel.saveHistoryServer(getFile(), nickname, result.first, GetDate().getTime().toString())
+                viewModel.saveHistoryServer(
+                    getFile(),
+                    nickname,
+                    result.first,
+                    GetDate().getTime().toString()
+                )
             }
 
         })
@@ -285,7 +317,12 @@ class CheckingFishFragment : Fragment() {
         viewModel.liveDataSaveAndWriteStatus.observe(viewLifecycleOwner, Observer {
 
             if (it) {
-                viewModel.saveHistoryServerAndChangeFragment(getFile(), nickname, result.first, GetDate().getTime().toString())
+                viewModel.saveHistoryServerAndChangeFragment(
+                    getFile(),
+                    nickname,
+                    result.first,
+                    GetDate().getTime().toString()
+                )
             }
 
         })
@@ -300,7 +337,7 @@ class CheckingFishFragment : Fragment() {
     } // observeLiveData()
 
 
-    fun getFile() : MultipartBody.Part {
+    fun getFile(): MultipartBody.Part {
 
         val storage = requireActivity().cacheDir
         val fileName = nickname + GetDate().getTime() + "jpg"
@@ -320,9 +357,9 @@ class CheckingFishFragment : Fragment() {
     } // getFile()
 
 
-    fun changeLayout(layout : String) {
+    fun changeLayout(layout: String) {
 
-        when(layout) {
+        when (layout) {
 
             "main" -> {
                 binding.checkingFishMainLayout.visibility = View.VISIBLE
@@ -330,18 +367,21 @@ class CheckingFishFragment : Fragment() {
                 binding.checkingFishDialogLayout.visibility = View.GONE
                 binding.checkingFishCheckImageLayout.visibility = View.GONE
             }
+
             "checkImage" -> {
                 binding.checkingFishMainLayout.visibility = View.GONE
                 binding.checkingFishResultLayout.visibility = View.GONE
                 binding.checkingFishDialogLayout.visibility = View.GONE
                 binding.checkingFishCheckImageLayout.visibility = View.VISIBLE
             }
+
             "result" -> {
                 binding.checkingFishMainLayout.visibility = View.GONE
                 binding.checkingFishResultLayout.visibility = View.VISIBLE
                 binding.checkingFishDialogLayout.visibility = View.GONE
                 binding.checkingFishCheckImageLayout.visibility = View.GONE
             }
+
             "complete" -> {
                 binding.checkingFishMainLayout.visibility = View.GONE
                 binding.checkingFishResultLayout.visibility = View.GONE
@@ -356,7 +396,8 @@ class CheckingFishFragment : Fragment() {
 
     fun checkUserShared() {
 
-        userInfoShared = requireActivity().getSharedPreferences("loginInfo", AppCompatActivity.MODE_PRIVATE)
+        userInfoShared =
+            requireActivity().getSharedPreferences("loginInfo", AppCompatActivity.MODE_PRIVATE)
         nickname = userInfoShared.getString("nickname", "").toString()
 
     } // checkUserShared()
@@ -364,7 +405,32 @@ class CheckingFishFragment : Fragment() {
 
     fun startCamera() {
 
-        camera.startScan()
+        TedPermission.create().setPermissionListener(object : PermissionListener {
+            override fun onPermissionGranted() {
+
+//                val fileName = nickname + System.currentTimeMillis()
+//                val storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+//                val file = File.createTempFile(fileName, ".jpg", storageDir)
+//
+//                captureUri = FileProvider.getUriForFile(requireActivity(), requireActivity().packageName+".fileprovider", file)
+//
+//                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                launcher.launch(intent)
+
+                val intent = Intent(requireActivity(), CameraActivity::class.java)
+                launcher.launch(intent)
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(requireActivity(), "권한을 허용해 주세요.", Toast.LENGTH_SHORT).show()
+                for (i in 0 until deniedPermissions!!.size) {
+                    Log.d(TAG, "onPermissionDenied: ${deniedPermissions[i]}")
+                }
+            }
+        }).setDeniedMessage("권한을 허용해 주세요.").setPermissions(android.Manifest.permission.CAMERA)
+            .check()
+
 
     } // startCamera()
 
@@ -374,12 +440,20 @@ class CheckingFishFragment : Fragment() {
         tensorflowModel.init()
         result = tensorflowModel.classify(resultBitmap)
 
-        binding.checkingFishCheckLayout.visibility = View.GONE
+        binding.checkingFishCheckImageLayout.visibility = View.GONE
         binding.checkingFishResultLayout.visibility = View.VISIBLE
         binding.checkingFishResultImage.setImageBitmap(resultBitmap)
         viewModel.getDescription(result.first, result.second)
 
 
     } // classify()
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        tensorflowModel.closeModel()
+
+    }
 
 }
