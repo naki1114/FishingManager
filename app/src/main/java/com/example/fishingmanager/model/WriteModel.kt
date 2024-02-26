33543ba85c6859445b6b731c7e6dcb1af5e6287b
Naki <naki1114@naver.com>
2024-02-26 16:54:15 +0900

    Chore : WriteFragment 기능 완료
    - 뒤로가기 버튼 클릭, 완료 버튼 클릭시 레이아웃 분리에 따른 로직 수정
    - 이미지 파일 유무에 따른 게시글 서버 전송 로직 완료

--- app/src/main/java/com/example/fishingmanager/model/WriteModel.kt
+++ app/src/main/java/com/example/fishingmanager/model/WriteModel.kt
@@ -2,11 +2,14 @@ package com.example.fishingmanager.model
 
 import com.example.fishingmanager.network.RetrofitClient
 import com.example.fishingmanager.network.RetrofitInterface
+import okhttp3.MultipartBody
 
 class WriteModel {
 
     private val responseServer = RetrofitClient.getWebServer().create(RetrofitInterface::class.java)
 
-    fun insertFeed(nickname : String, title : String, content : String, date : Int) = responseServer.insertFeed(nickname, title, content, date)
+    fun insertFeed(nickname : String, title : String, content : String, date : String) = responseServer.insertFeed(nickname, title, content, date)
+
+    fun insertImageFeed(file : MultipartBody.Part, nickname : String, title : String, content : String, date : String) = responseServer.insertImageFeed(file, nickname, title, content, date)
 
 }
