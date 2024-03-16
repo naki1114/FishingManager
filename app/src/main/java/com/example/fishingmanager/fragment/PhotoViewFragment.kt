4e949c6bccec10f014b3cf9f2378a497b407432b
Naki <naki1114@naver.com>
2024-03-17 01:34:38 +0900

    Refactor : PhotoViewFragment 리팩토링

--- app/src/main/java/com/example/fishingmanager/fragment/PhotoViewFragment.kt
+++ app/src/main/java/com/example/fishingmanager/fragment/PhotoViewFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.example.fishingmanager.R
 import com.example.fishingmanager.activity.MainActivity
@@ -16,34 +15,39 @@ import com.example.fishingmanager.databinding.FragmentPhotoViewBinding
 import com.example.fishingmanager.network.RetrofitClient
 import com.example.fishingmanager.viewModel.PhotoViewViewModel
 
-class PhotoViewFragment : Fragment() {
+class PhotoViewFragment: Fragment() {
 
-    lateinit var binding : FragmentPhotoViewBinding
-    lateinit var viewModel : PhotoViewViewModel
+    lateinit var binding: FragmentPhotoViewBinding
+    lateinit var viewModel: PhotoViewViewModel
 
     override fun onAttach(context: Context) {
+
         super.onAttach(context)
 
-        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+        requireActivity().onBackPressedDispatcher.addCallback(object: OnBackPressedCallback(true) {
+
             override fun handleOnBackPressed() {
 
                 (activity as MainActivity).removeFragmentStack()
                 (activity as MainActivity).navigationVisible()
 
             }
+
         })
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_photo_view, container, false)
+
         return binding.root
-    }
+
+    } // onCreateView()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
 
         setVariable()
@@ -52,6 +56,7 @@ class PhotoViewFragment : Fragment() {
     } // onViewCreated()
 
 
+    // 초기화
     fun setVariable() {
 
         viewModel = PhotoViewViewModel()
@@ -67,26 +72,34 @@ class PhotoViewFragment : Fragment() {
     } // setVariable()
 
 
-    fun observeLiveData() {
+    // LiveData observe
+    private fun observeLiveData() {
 
-        viewModel.liveDataCloseStatus.observe(viewLifecycleOwner, Observer {
+        viewModel.liveDataCloseStatus.observe(viewLifecycleOwner) {
 
             if (it) {
+
                 (activity as MainActivity).onBackPressed()
+
             }
 
-        })
+        }
 
-        viewModel.liveDataImage.observe(viewLifecycleOwner, Observer {
+        viewModel.liveDataImage.observe(viewLifecycleOwner) {
 
             if (it == "FM") {
+
                 Glide.with((activity as MainActivity)).load(R.drawable.fishing_logo).into(binding.photoViewImage)
+
             } else {
+
                 Glide.with((activity as MainActivity)).load(RetrofitClient.BASE_URL + it).into(binding.photoViewImage)
+
             }
 
-        })
+        }
 
     } // observeLiveData()
+
 
 }
--- app/src/main/java/com/example/fishingmanager/viewModel/PhotoViewViewModel.kt
+++ app/src/main/java/com/example/fishingmanager/viewModel/PhotoViewViewModel.kt
@@ -3,11 +3,13 @@ package com.example.fishingmanager.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PhotoViewViewModel : ViewModel() {
+class PhotoViewViewModel: ViewModel() {
 
     val liveDataCloseStatus = MutableLiveData<Boolean>()
     val liveDataImage = MutableLiveData<String>()
 
+
+    // 프래그먼트 닫기
     fun close() {
 
         liveDataCloseStatus.value = true
@@ -15,10 +17,12 @@ class PhotoViewViewModel : ViewModel() {
     } // close()
 
 
-    fun setImage(image : String) {
+    // 이미지 적용
+    fun setImage(image: String) {
 
         liveDataImage.value = image
 
-    } // setImage
+    } // setImage()
+
 
 }
